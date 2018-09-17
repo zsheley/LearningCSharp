631a9b85eb29ce892ec04a23033cd156e9e4aa00
Zachery Sheley <zsheley@gsgis.k12.va.us>
2018-09-17 14:28:51 -0400

    Update C#-ElementsExample.cs

--- C#-ElementsExample.cs
+++ C#-ElementsExample.cs
@@ -1,14 +1,19 @@
+/* Programmer: [Zachery Sheley]
+ * Date: [9/17/18]
+ * Purpose: This class can be used to send messages to the output screen
+ * */
+
 using System;
 using static System.Console;
 namespace ProgrammingMessage
 {
-  class ProgrammingMessage
-  {
-    static void Main()
+    class ProgrammingMessage
     {
-      WriteLine("Programming can be";
-      WriteLine("FUN!");
-      ReadKey();
-      }
-   }
+        static void Main()
+        {
+            WriteLine("Programming can be");
+            WriteLine("FUN!");
+            ReadKey();
+        }
+    }
 }
