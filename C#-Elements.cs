343b4adeaab843486a2086e4f4a664634890a0da
Zachery Sheley <zsheley@gsgis.k12.va.us>
2018-09-17 14:17:24 -0400

    Create C#-Elements.cs

---
+++ C#-Elements.cs
@@ -0,0 +1,14 @@
+using System;
+using static System.Console;
+namespace ProgrammingMessage
+{
+  class ProgrammingMessage
+  {
+    static void Main()
+    {
+      WriteLine("Programming can be";
+      WriteLine("FUN!");
+      ReadKey();
+      }
+   }
+}
